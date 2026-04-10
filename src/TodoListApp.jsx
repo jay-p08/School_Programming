5b50e559fa97ef1851373d52b11cc40e1143350d
jay-p08 <soRjdla0709@gmail.com>
2026-04-10 14:37:30 +0900

    TodoListApp HTML className, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,25 +1,28 @@
-import './reset.css';
+import './todolist.css';
 
 function TodoListApp() {
     return (
-        <div>
-            <h1>Sudo List App</h1>
+        <div className = 'todo'>
+            <h1 className = 'todo__title'>Sudo List App</h1>
 
-            <form>
-                <input type="text" />
-                <button type="submit">Add</button>
+            <form className = 'todo__form'>
+                <input type="text" className='todo__input' placeholder='할 일을 입력하세요.'/>
+                <button type="submit" className='todo__button todo_button--add'>Add</button>
             </form>
 
-            <ul>
-                <li>
-                    <label ><input type="checkbox" id=""/>지각 하기</label>
-                    <button>🔮</button>
-                    <button>✖️</button>
+            <ul className='todo__list'>
+                <li className='todo__item todo__item--empty'>
+                    <p>할 일 없음.</p>
                 </li>
-                <li>
-                    <label><input type="checkbox" id=""/>롤체 in down</label>
-                    <button>🔮</button>
-                    <button>✖️</button>
+                <li className='todo__item todo__item--complete'>
+                    <label className='todo__label'><input type="checkbox" className='todo__check'/>지각 하기</label>
+                    <button className='todo__button todo__button--edit'>🔮</button>
+                    <button className='todo__button todo__button--delete'>✖️</button>
+                </li>
+                <li className='todo__item'>
+                    <label className='todo__label'><input type="checkbox" className='todo__check'/>롤체 in down</label>
+                    <button className='todo__button todo__button--edit'>🔮</button>
+                    <button className='todo__button todo__button--delete'>✖️</button>
                 </li>
             </ul> 
         </div> // div
